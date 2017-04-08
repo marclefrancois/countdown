b18e3f32981be90cc205f4231b41f04b0a0b4705
Marc Lefrancois <mlefrancois@mirego.com>
2017-04-08 10:28:20 -0400

    add build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+npm run build
+rm -rf ~/Documents/GIT/marclefrancois.github.io/*
+cp -R build/ ~/Documents/GIT/marclefrancois.github.io/
+cd ~/Documents/GIT/marclefrancois.github.io/
+git add .
+git commit -m "deploy $(date "+%FT%T")"
+git push
+cd ~/Documents/GIT/countdown/
