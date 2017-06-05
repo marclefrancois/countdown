f388c7a11395b872ab8bbe575b63565fd652038d
Marc Lefrancois <mlefrancois@mirego.com>
2017-06-05 10:37:11 -0400

    add version info to deploy commit

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-node version-number.js
 yarn version
+newVersion=$(node version-number.js)
 yarn build
 rm -rf ~/Documents/GIT/marclefrancois.github.io/*
 cp -R build/ ~/Documents/GIT/marclefrancois.github.io/
 cd ~/Documents/GIT/marclefrancois.github.io/
 git add .
-git commit -m "deploy $(date "+%FT%T")"
+git commit -m "deploy $(date "+%FT%T") $newVersion"
 git push
 cd ~/Documents/GIT/countdown/
 git push
