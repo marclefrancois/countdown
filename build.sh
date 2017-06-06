5d61567eb5cb3cd726b5c742ae2666e74d1d3f4a
Marc Lefrancois <mlefrancois@mirego.com>
2017-06-06 17:50:00 -0400

    nicer deploy commit

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf ~/Documents/GIT/marclefrancois.github.io/*
 cp -R build/ ~/Documents/GIT/marclefrancois.github.io/
 cd ~/Documents/GIT/marclefrancois.github.io/
 git add .
-git commit -m "deploy $(date "+%FT%T") $newVersion"
+git commit -m "deploy version $newVersion ($(date "+%FT%T"))"
 git push
 cd ~/Documents/GIT/countdown/
 git push
