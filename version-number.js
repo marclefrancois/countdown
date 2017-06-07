5ea07a12efe50fff470837474126e9847b342434
Marc Lefrancois <mlefrancois@mirego.com>
2017-06-06 22:45:22 -0400

    Feature/auto increment build number (#3)
    * auto-increment-build number
    
    * WIP

--- version-number.js
+++ version-number.js
@@ -1,2 +1,4 @@
-var pjson = require('./package.json');
-console.log(pjson.version);
+let versionSplitter = '.'
+let packageInfo = require('./package.json');
+let versionInfo = packageInfo.version.split(versionSplitter)
+console.log(versionInfo[0] + versionSplitter + versionInfo[1] + versionSplitter + (parseInt(versionInfo[2]) + 1));
