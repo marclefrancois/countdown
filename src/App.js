55fcee7af462b64c25d55d6cdb4938570b4c6ac2
Marc Lefrancois <mlefrancois@mirego.com>
2017-11-14 22:44:10 -0500

    winterfestWIP

--- src/App.js
+++ src/App.js
@@ -3,9 +3,48 @@ import './App.css';
 import CountdownBox from './views/countdown-box/countdown-box'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageUrl: 'official.jpg'
+    }
+  }
+
+  componentDidMount() {
+    this._setBackgroundStyle()
+    this.timerID = setInterval(
+      () => this._tick(),
+      10000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  _tick() {
+    this.setState({
+      imageUrl: 'winterfest2017.jpg'
+    });
+    this._setBackgroundStyle()
+    clearInterval(this.timerID);
+  }
+
+  _setBackgroundStyle() {
+    const imageUrl =`url(${this.state.imageUrl})`;
+    this.setState({
+      backgroundStyle : {
+        backgroundImage : imageUrl,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }
+    });
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" style={this.state.backgroundStyle}>
         <CountdownBox/>
       </div>
     );
--- src/views/countdown-box/countdown-box.js
+++ src/views/countdown-box/countdown-box.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './countdown-box.css';
 import MiregoLogo from '../../images/mirego.png'
 import Ticker from '../countdown-ticker/ticker'
-import Flotation from '../flotation-call-to-action/flotation'
 
 class CountdownBox extends Component {
   render() {
@@ -10,7 +9,8 @@ class CountdownBox extends Component {
       <div className="countdown-box">
         <img className="countdown-image" src={MiregoLogo} alt="Logo de Mirego"/>
         <div className="countdown-title">Notre Winterfest est dans</div>
-        <Ticker countDownToDate='2017-12-22T16:00:00Z'/>
+        <Ticker countDownToDate='2017-12-08T14:00:00Z'/>
+        <div className="countdown-title">Are you stoked ?</div>
       </div>
     );
   }
