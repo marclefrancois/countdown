55003afc65d03dc294691bf0b00280b9d6506289
Marc Lefrancois <marclefrancois@Marcs-MacBook-Pro.local>
2022-07-05 17:18:15 -0400

    Remove big mirego logo

--- src/views/countdown-box/countdown-box.js
+++ src/views/countdown-box/countdown-box.js
@@ -5,15 +5,12 @@ import SummerLogo from './logo-summerfest.png'
 import MiregoLogo from './logo-mirego.png'
 import WinterLogo from './logo-winterfest.png'
 import Ticker from '../countdown-ticker/ticker'
-import BigMirego from './mirego.png'
 
 class CountdownBox extends Component {
   render() {
     const logo = this.props.isWinter ? WinterLogo: SummerLogo;
     return (
       <div className="countdown-box">
-        <img className="countdown-image" src={BigMirego} alt="Logo de Mirego"/>
-        
         <img className="countdown-image" src={logo} alt="Summerfest things"/>
         <div className="countdown-title">Le Summerfest est dans</div>
         <Ticker countDownToDate={this.props.countdownToDate}/>
